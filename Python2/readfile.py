da48d10dc503969f296ea2e0f6520ba9ab2d2f23
Devinaille <devinailletian@163.com>
2015-02-15 15:43:54 +0800

    笔记代码
    笔记代码

--- Python2/readfile.py
+++ Python2/readfile.py
@@ -3,5 +3,12 @@ path = r'C:\Users\tianyf\Desktop\优化.txt'.decode('utf-8')
 print path
 f = open(path, 'r')
 for line in f:
-  print(line.decode('utf-8'))
+  print(line.decode('utf-8')),
+  # 逗号表示不换行，在3.X中为print(str1, end="")
 f.close()
+
+# enmerate可以显示序号
+f1 = open(path, 'r')
+for i, line in enumerate(f1):
+  print i, line.decode('utf-8'),
+f1.close()
